e2175fe5f592c82ca418a3fcb0ade125a435a856
Sri Raj Kumar Yaramsetti <srirajkumaryaramsetti10@gmail.com>
2026-03-29 11:20:33 -0400

    firstcommit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class EmployeeController {
+
+    @GetMapping("/api/employees")
+    public List<Employee> getEmployees() {
+        return Arrays.asList(
+                new Employee(1, "Sri Raj", "Java Developer"),
+                new Employee(2, "John", "React Developer")
+        );
+    }
+}
